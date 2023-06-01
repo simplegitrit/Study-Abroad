8ebedffae9bd2c72817b44a2a1bae5d3280cc34f
simplegitrit <127583449+simplegitrit@users.noreply.github.com>
2023-06-01 20:30:08 +0530

    commit

---
+++ vasant.php
@@ -0,0 +1,179 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="shortcut icon" type="png" href="images/icon/favicon.png">
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Comaptible" content="IE=edge">
+	<title>Study Abroad</title>
+	<meta name="desciption" content="">
+   
+	<link rel="stylesheet" type="text/css" href="style1.css">
+	<script type="text/javascript" src="script.js"></script>
+	<script src="https://code.jquery.com/jquery-3.2.1.js"></script>
+	<script>
+		$(window).on('scroll', function(){
+  			if($(window).scrollTop()){
+  			  $('nav').addClass('black');
+ 			 }else {
+ 		   $('nav').removeClass('black');
+ 		 }
+		})
+	</script>
+</head>
+<body>
+<!-- Navigation Bar -->
+	<header id="header">
+		<nav>
+			<div class="logo"><img src="images/icon/Study Abroad-logos_ (1).jpeg"></div>
+			<ul>
+				<li><a href="vasant.php?">Home</a></li>
+				<li><a href="#portfolio_section">Portfolio</a></li>
+				<li><a href="#team_section">Team</a></li>
+				<li><a href="#services_section">Services</a></li>
+				<li><a href="contact.html">Contact</a></li>
+			</ul>
+			<div class="srch"><input type="text" class="search" placeholder="Search here..."><img src="images/icon/search.png" alt="search" onclick=slide()></div>
+			<a class="get-started" href="login.html">Get Started</a>
+			<img src="images/icon/menu.png" class="menu" onclick="sideMenu(0)" alt="menu">
+		</nav>
+		<div class="head-container">
+			<div class="quote">
+				<h1>The beautiful thing about learning is that nobody can take it away from you</h1>
+				
+					<img src="images/icon/play.png" alt="play"><span><a href="https://www.youtube.com/watch?v=KFyrgDO1WXk" target="_blank">Watch Now</a></span>
+				</div> 
+			</div>
+			<div class="image">
+				<img src="Learning.gif" alt="svg">
+				
+			</div>
+		</div>
+		<div class="side-menu" id="side-menu">
+			<div class="close" onclick="sideMenu(1)"><img src="images/icon/close.png" alt=""></div>
+			<div class="user">
+				<img src="images/creator/roshan.jpeg" alt="Username">
+				<p>roshank9419</p>
+			</div>
+			<ul>
+				<li><a href="#about_section">About</a></li>
+				<li><a href="#portfolio_section">Portfolio</a></li>
+				<li><a href="#team_section">Team</a></li>
+				<li><a href="#services_section">Services</a></li>
+				<li><a href="#contactus_section">Contact</a></li>
+				<li><a href="#feedBACK">Feedback</a></li>
+			</ul>
+		</div>
+	</header>
+
+ 
+
+		<div class="diffSection" id="about_section">
+			<center><p style="font-size: 50px; padding: 100px ; padding-bottom: 20px;"></p></center>
+			<div class="about-content">
+					<div class="side-image">
+						<img class="sideImage" src="Knowledge.gif">
+					</div>
+					<div class="side-text">
+						<h2>What you think about us ?</h2>
+						<p>Education is the process of facilitating learning, or the acquisition of knowledge, skills, values, beliefs, and habits. Educational methods include teaching, training, storytelling, discussion and directed research.<br> Educational website can include websites that have games, videos or topic related resources that act as tools to enhance learning and supplement classroom teaching. These websites help make the process of learning entertaining and attractive to the student, especially in today's age. <br>Using HTML(HyperText Markup Language), CSS(Cascading Style Sheet), JavaScript, we can make learning more easier and in a interesting way.</p>
+					</div>
+			</div>
+		</div>
+		
+		<div>
+
+
+		
+			<link rel="stylesheet" href="drag.css">
+			<meta name="viewport" content="width=device-width, initial-scale=1.0">
+			<!-- Font Awesome CDN Link for Icons -->
+			<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
+			<script src="drag.js" defer></script>
+		  </head>
+		  <body>
+		  <div class="sonu">
+			<h1 style="text-align: center;  position:relative ;bottom:400px;">Choose the country</h1>
+                </div>
+
+			<div class="wrapper">
+			  <div class="icon"><i id="left" class="fa-solid fa-angle-left"></i></div>
+			  <ul class="tabs-box">
+				<a href="london/london.html"><li class="tab active">London</li></a>
+				<a href="london/germany.html"><li class="tab active">Germany</li></a>
+				<a href="london/canada.html"><li class="tab active">Canada</li></a>
+				<a href="london/USA.html"><li class="tab active">USA</li></a>
+				<a href="london/ireland.html"><li class="tab active">Ireland</li></a>
+				<a href="london/russia.html"><li class="tab active">Russia</li></a> 
+				<a href="london/singapore.html"><li class="tab active">Singapore</li></a> 
+				<li class="tab">Gadgets</li>
+				<li class="tab">Algorithms</li>
+				<li class="tab">Comedy</li>
+				<li class="tab">Gaming</li>
+				<li class="tab">Share Market</li>
+				<li class="tab">Smartphones</li>
+				<li class="tab">Data Structure</li>
+			  </ul>
+			  <div class="icon"><i id="right" class="fa-solid fa-angle-right"></i></div>
+			</div>
+
+			<div class="diffSection" id="about_section">
+			<center><p style="font-size: 50px; padding: 100px ; padding-bottom: 20px;"></p></center>
+			<div class="about-content">
+					<div class="side-image">
+						<img class="sideImage" src="Knowledge.gif">
+					</div>
+					<div class="side-text">
+						<h2>What you think about us ?</h2>
+						<p>Education is the process of facilitating learning, or the acquisition of knowledge, skills, values, beliefs, and habits. Educational methods include teaching, training, storytelling, discussion and directed research.<br> Educational website can include websites that have games, videos or topic related resources that act as tools to enhance learning and supplement classroom teaching. These websites help make the process of learning entertaining and attractive to the student, especially in today's age. <br>Using HTML(HyperText Markup Language), CSS(Cascading Style Sheet), JavaScript, we can make learning more easier and in a interesting way.</p>
+					</div>
+			</div>
+		</div>
+		
+		
+
+	
+		<div class="service-swipe">
+			<div class="diffSection" id="services_section">
+			<center><h1 style="font-size: 50px; padding: 100px; padding-bottom: 40px; color: black; ">Services</h1></center>
+			</div>
+			<a href="subjects/computer_courses.html"><div class="s-card"><img src="images/icon/computer-courses.png"><p>Free Online Computer Courses</p></div></a> 
+			<a href="subjects/jee.html"><div class="s-card"><img src="images/icon/brainbooster.png"><p>Building Concepts for Competitive Exams</p></div></a>
+			<a href="#"><div class="s-card"><img src="images/icon/online-tutorials.png"><p>Online Video Lectures</p></div></a>
+			<a href="subjects/jee.html#sample_papers"><div class="s-card"><img src="images/icon/papers.jpg"><p>Sample Papers of Various Competitive Exams</p></div></a>
+			<a href="todo.html"><div class="s-card"><img src="Checklist-pana.png"><p>To do list</p></div></a>
+			<a href="mainbot.php?"><div class="s-card"><img src="images/icon/discussion.png"><p>Discussion with Our Tutors & Mentors</p></div></a>
+			<a href="subjects/quiz.html"><div class="s-card"><img src="images/icon/q1.png"><p>Daily Brain Teasing Questions to Improve IQ</p></div></a>
+			<a href="#contactus_section"><div class="s-card"><img src="images/icon/help.png"><p>Buy courses</p></div></a> 
+		</div>
+	
+		<footer>
+			<div class="footer-container">
+				<div class="left-col">
+					<img src="images/icon/Study Abroad-logos_ (1).jpeg" style="width: 200px;">
+					<div class="logo"></div>
+					<div class="social-media">
+						<a href="#"><img src="images/icon\fb.png"></a>
+						<a href="#"><img src="images/icon\insta.png"></a>
+						<a href="#"><img src="images/icon\tt.png"></a>
+						<a href="#"><img src="images/icon\ytube.png"></a>
+						<a href="#"><img src="images/icon\linkedin.png"></a>
+					</div><br><br>
+					<p class="rights-text">Project Created by team WOW.</p>
+					<br><p><img src="images/icon/location.png"> Saraswati college of engineering<br>Kharghar, kharghar-410210</p><br>
+					<p><img src="images/icon/phone.png"> +91-1234-567-890<br><img src="images/icon/mail.png">&nbsp; 	wow123@gmail.com</p>
+				</div>
+				<div class="right-col">
+					<h1 style="color: #fff">Our Newsletter</h1>
+					<div class="border"></div><br>
+					<!-- <p>Enter Your Email to get our News and updates.</p> -->
+					<form class="newsletter-form">
+						<input class="txtb" type="email" placeholder="Enter Your Email">
+						<input class="btn" type="submit" value="Submit">
+					</form>
+				</div>
+			</div>
+		</footer>
+	
+	  </body>
+	</html>
+	
